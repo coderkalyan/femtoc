a51bbf6c65129c9c0a276c0c5f2f7e28439f07c0
Kalyan Sriram <kalyan@coderkalyan.com>
2023-03-13 11:46:13 -0700

    mir: unify analyzer and mirgen
    Extracts module MIR generation into Analyzer.zig. Removes the switch in
    mirgen, and replaces mir's Module struct with a global Compilation.

--- src/MirGen.zig
+++ src/MirGen.zig
@@ -5,6 +5,7 @@ const Type = @import("typing.zig").Type;
 const Analyzer = @import("Analyzer.zig");
 const MirMap = @import("MirMap.zig");
 const Module = @import("Module.zig");
+const Compilation = @import("Compilation.zig");
 
 const math = std.math;
 
@@ -18,13 +19,11 @@ gpa: Allocator,
 arena: Allocator,
 
 hir: *const Hir,
-insts: std.MultiArrayList(Mir.Inst),
-extra: std.ArrayListUnmanaged(u32),
-values: std.ArrayListUnmanaged(Value),
-scratch: std.ArrayListUnmanaged(u32),
 map: MirMap,
+global: ?Mir,
+mir: std.ArrayListUnmanaged(Mir),
 
-pub fn generate(gpa: Allocator, hir: *const Hir) !Module {
+pub fn generate(gpa: Allocator, hir: *const Hir) !Compilation {
     var arena = std.heap.ArenaAllocator.init(gpa);
     defer arena.deinit();
 
@@ -32,18 +31,16 @@ pub fn generate(gpa: Allocator, hir: *const Hir) !Module {
         .gpa = gpa,
         .arena = arena.allocator(),
         .hir = hir,
-        .insts = .{},
-        .extra = .{},
-        .values = .{},
-        .scratch = .{},
         .map = MirMap.init(null),
+        .global = null,
+        .mir = .{},
     };
-    return mirgen.walkModule();
-    // return Mir {
-    //     .insts = mirgen.insts.toOwnedSlice(),
-    //     .extra = mirgen.extra.toOwnedSlice(gpa),
-    //     .values = mirgen.values.toOwnedSlice(gpa),
-    // };
+    const global = try mirgen.walkModule();
+
+    return Compilation {
+        .global = global,
+        .mir = mirgen.mir.toOwnedSlice(gpa),
+    };
 }
 
 pub fn addInst(mg: *MirGen, inst: Mir.Inst) !Mir.Index {
@@ -90,259 +87,27 @@ pub fn resolveTy(mg: *MirGen, ref: Mir.Ref) !Type {
     }
 }
 
-pub fn walkModule(mg: *MirGen) !Module {
-    const hir_module = mg.hir.insts.items(.data)[mg.hir.insts.len - 1];
-    const module_pl = hir_module.pl_node.pl;
-    const data = mg.hir.extraData(module_pl, Hir.Inst.Module);
+pub fn walkModule(mg: *MirGen) !Mir {
+    var analyzer = Analyzer {
+        .mg = mg,
+        .map = MirMap.init(&mg.map),
+        .hir = mg.hir,
+        .gpa = mg.gpa,
+        .arena = mg.arena,
+        .instructions = .{},
+        .extra = .{},
+        .values = .{},
+        .scratch = .{},
+        .errors = .{},
+        .interner = &mg.hir.interner,
+    };
 
-    var function_mir = std.ArrayListUnmanaged(Mir){};
-    const scratch_top = mg.scratch.items.len;
-    defer mg.scratch.shrinkRetainingCapacity(scratch_top);
+    try analyzer.analyzeModule(@intCast(u32, mg.hir.insts.len - 1));
 
-    try mg.map.ensureSliceCapacity(mg.arena, mg.hir.extra_data[module_pl + 1..module_pl + 1 + data.len]);
-    var extra_index: u32 = 0;
-    while (extra_index < data.len) : (extra_index += 1) {
-        const hir_index = mg.hir.extra_data[module_pl + 1 + extra_index];
-        switch (mg.hir.insts.items(.tag)[hir_index]) {
-            .int => {
-                const value = try mg.addValue(.{ .uint = mg.hir.insts.items(.data)[hir_index].int });
-                const index = try mg.addInst(.{
-                    .tag = .constant,
-                    .data = .{
-                        .ty_pl = .{
-                            .ty = .{ .tag = .comptime_uint },
-                            .pl = value,
-                        }
-                    }
-                });
-                try mg.scratch.append(mg.arena, index);
-                mg.map.putAssumeCapacity(hir_index, Mir.indexToRef(index));
-            },
-            .float => {
-                const value = try mg.addValue(.{
-                    .float = mg.hir.insts.items(.data)[hir_index].float
-                });
-                const index = try mg.addInst(.{
-                    .tag = .constant,
-                    .data = .{
-                        .ty_pl = .{
-                            .ty = .{ .tag = .comptime_float },
-                            .pl = value,
-                        }
-                    }
-                });
-                try mg.scratch.append(mg.arena, index);
-                mg.map.putAssumeCapacity(hir_index, Mir.indexToRef(index));
-            },
-            .validate_ty => {
-                const pl = mg.hir.insts.items(.data)[hir_index].pl_node.pl;
-                const validate_ty = mg.hir.extraData(pl, Hir.Inst.ValidateTy);
-                const ref = mg.map.resolveRef(validate_ty.ref);
-                const found_ty = try mg.resolveTy(ref);
-                const expected_ty = validate_ty.ty.toType();
-
-                if (found_ty.tag == expected_ty.tag) {
-                    mg.map.putAssumeCapacity(hir_index, ref);
-                } else {
-                    const coerce_ref = switch (found_ty.tag) {
-                        .comptime_uint => ref: {
-                            const value_index = Mir.refToIndex(ref).?;
-                            const int_value = mg.hir.insts.items(.data)[value_index].int;
-                            const value = try mg.addValue(.{ .uint = int_value });
-                            const index = try mg.addInst(.{
-                                .tag = .constant,
-                                .data = .{
-                                    .ty_pl = .{
-                                        .ty = expected_ty,
-                                        .pl = value,
-                                    }
-                                }
-                            });
-                            try mg.scratch.append(mg.arena, index);
-                            break :ref Mir.indexToRef(index);
-                        },
-                        else => return Error.TypeError,
-                    };
-                    mg.map.putAssumeCapacity(hir_index, coerce_ref);
-                }
-            },
-            .fn_decl => {
-                // const pl = mg.hir.insts.items(.data)[hir_index].pl_node.pl;
-                // const fn_decl = mg.hir.extraData(pl, Hir.Inst.FnDecl);
-
-                var analyzer = Analyzer {
-                    .mg = mg,
-                    .map = MirMap.init(&mg.map),
-                    .hir = mg.hir,
-                    .gpa = mg.gpa,
-                    .arena = mg.arena,
-                    .instructions = .{},
-                    .extra = .{},
-                    .values = .{},
-                    .scratch = .{},
-                    .errors = .{},
-                    .interner = &mg.hir.interner,
-                };
-
-                // const mir = try analyzer.analyzeBody(fn_decl.body);
-                const mir = try analyzer.analyzeFunction(hir_index);
-                try function_mir.append(mg.gpa, mir);
-            },
-            .alloc => {
-                const op = mg.hir.insts.items(.data)[hir_index].un_node.operand;
-                const ref = mg.map.resolveRef(op);
-                const ty = try mg.resolveTy(ref);
-                const index = try mg.addInst(.{
-                    .tag = .alloc,
-                    .data = .{ .ty = ty },
-                });
-                try mg.scratch.append(mg.arena, index);
-                mg.map.putAssumeCapacity(hir_index, Mir.indexToRef(index));
-            },
-            // else => {},
-            else => std.debug.print("unimplemented: {}\n", .{mg.hir.insts.items(.tag)[hir_index]}),
-        }
-    }
-
-    const insts = mg.scratch.items[scratch_top..];
-    const module_data = try mg.addExtra(Mir.Module {
-        .len = @intCast(u32, insts.len),
-    });
-    try mg.extra.appendSlice(mg.gpa, insts);
-    const module_inst = try mg.addInst(.{
-        .tag = .module,
-        .data = .{ .pl = module_data },
-    });
-    _ = module_inst;
-
-    return Module {
-        .function_mir = function_mir.toOwnedSlice(mg.gpa),
+    return Mir {
+        .insts = analyzer.instructions.toOwnedSlice(),
+        .extra = analyzer.extra.toOwnedSlice(analyzer.gpa),
+        .values = analyzer.values.toOwnedSlice(analyzer.gpa),
+        .interner = analyzer.interner,
     };
 }
-    //
-    // fn call(mg: *MirGen, inst: Hir.Inst) !Mir.Ref {
-    //     const hir_pl = inst.data.pl_node.pl;
-    //     const hir_call = mg.hir.extraData(hir_pl, Hir.Inst.Call);
-    //
-    //     const pl = mg.addExtra(mir.Inst.Call {
-    //         .args_len = hir_call.args_end - hir_call.args_start,
-    //     });
-    //     const args = mg.hir.extra_data[hir_call.args_start..hir_call.args_end];
-    //     try mg.extra.appendSlice(args);
-    //
-    //     return try mg.addInst(.{
-    //         .tag = .call,
-    //         .data = .{
-    //             .op_pl = .{
-    //                 .op = mir.Inst.indexToRef(0), // TODO
-    //                 .pl = pl,
-    //             }
-    //         }
-    //     });
-    // }
-    //
-    // fn binary(mg: *MirGen, inst: Hir.Inst) !Mir.Ref {
-    //     const hir_pl = inst.data.pl.pl_node;
-    //     const hir_bin = mg.hir.extraData(hir_pl, Hir.Inst.Binary);
-    //     const lref = mg.resolveInst(hir_bin.lref);
-    //     const rref = mg.resolveInst(hir_bin.rref);
-    //     const lty = mg.resolveTy(lref);
-    //     const rty = mg.resolveTy(rref);
-    //
-    //     if ((lty.tag == .comptime_int) and (rty.tag == .comptime_int)) {
-    //         const lpl = mg.insts[mir.Inst.refToIndex(lref)].data.ty_pl.pl;
-    //         const rpl = mg.insts[mir.Inst.refToIndex(rref)].data.ty_pl.pl;
-    //         const lval = mg.values[lpl];
-    //         const rval = mg.values[rpl];
-    //         return mg.addInst(.{
-    //             .tag = .constant,
-    //             .data = .{
-    //                 .ty_pl = .{
-    //                     .ty = .comptime_int,
-    //                     .pl = try mg.addValue(.{ .int = lval + rval }),
-    //                 }
-    //             }
-    //         });
-    //     } else if (lty.tag == .comptime_int) {
-    //         const lpl = mg.insts[mir.Inst.refToIndex(lref)].data.ty_pl.pl;
-    //         const lval = mg.values[lpl];
-    //         const tv = TypedValue {
-    //             .ty = .{ .tag = .comptime_int },
-    //             .val = .{ .int = @bitCast(u64, lval) },
-    //         };
-    //
-    //         if (tv.coerce(rty)) |result| {
-    //             const ref = mg.addInst(.{
-    //                 .tag = .constant,
-    //                 .data = .{
-    //                     .ty_pl = .{
-    //                         .ty = result.ty,
-    //                         .pl = try mg.addValue(.{ .int = result.val }),
-    //                     }
-    //                 },
-    //             });
-    //             return mg.addInst(.{
-    //                 .tag = switch (inst.tag) {
-    //                     .add => .add,
-    //                     .sub => .sub,
-    //                     .mul => .mul,
-    //                     .div => .div,
-    //                     .eq => .eq,
-    //                     .neq => .neq,
-    //                     else => unreachable,
-    //                 },
-    //                 .data = .{ .bin_op = .{ .lref = lref, .rref = ref } },
-    //             });
-    //         }
-    //     } else if (rty.tag == .comptime_int) {
-    //         const rpl = mg.insts[mir.Inst.refToIndex(rref)].data.ty_pl.pl;
-    //         const rval = mg.values[rpl];
-    //         const tv = TypedValue {
-    //             .ty = .{ .tag = .comptime_int },
-    //             .val = .{ .int = @bitCast(u64, rval) },
-    //         };
-    //
-    //         if (tv.coerce(lty)) |result| {
-    //             const ref = mg.addInst(.{
-    //                 .tag = .constant,
-    //                 .data = .{
-    //                     .ty_pl = .{
-    //                         .ty = result.ty,
-    //                         .pl = try mg.addValue(.{ .int = result.val }),
-    //                     }
-    //                 },
-    //             });
-    //             return mg.addInst(.{
-    //                 .tag = switch (inst.tag) {
-    //                     .add => .add,
-    //                     .sub => .sub,
-    //                     .mul => .mul,
-    //                     .div => .div,
-    //                     .eq => .eq,
-    //                     .neq => .neq,
-    //                     else => unreachable,
-    //                 },
-    //                 .data = .{ .bin_op = .{ .lref = ref, .lref = rref } },
-    //             });
-    //         }
-    //     } else {
-    //         switch (lty.tag) {
-    //             .u1, .i8, .u8, .i16, .u16, .i32, .u32, .i64, .u64 => {
-    //                 if (lty.tag != rty.tag) return Error.TypeError;
-    //                 return mg.addInst(.{
-    //                     .tag = switch (inst.tag) {
-    //                         .add => .add,
-    //                         .sub => .sub,
-    //                         .mul => .mul,
-    //                         .div => .div,
-    //                         .eq => .eq,
-    //                         .neq => .neq,
-    //                         else => unreachable,
-    //                     },
-    //                     .data = .{ .bin_op = .{ .lref = lref, .rref = rref } },
-    //                 });
-    //             },
-    //             else => return Error.TypeError,
-    //         }
-    //     }
-    // }
