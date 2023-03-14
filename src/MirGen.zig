2ba10d8e9f31a45302c9825c95c401ea447d115a
Kalyan Sriram <kalyan@coderkalyan.com>
2023-03-13 17:14:11 -0700

    mirgen: remove unused functions

--- src/MirGen.zig
+++ src/MirGen.zig
@@ -43,50 +43,6 @@ pub fn generate(gpa: Allocator, hir: *const Hir) !Compilation {
     };
 }
 
-pub fn addInst(mg: *MirGen, inst: Mir.Inst) !Mir.Index {
-    const result = @intCast(Mir.Index, mg.insts.len);
-    try mg.insts.append(mg.gpa, inst);
-    return result;
-}
-
-pub fn addExtra(mg: *MirGen, extra: anytype) !u32 {
-    const fields = std.meta.fields(@TypeOf(extra));
-    try mg.extra.ensureUnusedCapacity(mg.gpa, fields.len);
-    const len = @intCast(u32, mg.extra.items.len);
-    inline for (fields) |field| {
-        comptime std.debug.assert(field.field_type == u32);
-        mg.extra.appendAssumeCapacity(@field(extra, field.name));
-    }
-
-    return len;
-}
-
-pub fn addValue(mg: *MirGen, val: Value) !u32 {
-    const len = @intCast(u32, mg.values.items.len);
-    try mg.values.append(mg.gpa, val);
-    return len;
-}
-
-pub fn resolveTy(mg: *MirGen, ref: Mir.Ref) !Type {
-    if (Mir.refToIndex(ref)) |index| {
-        return switch (mg.insts.items(.tag)[index]) {
-            .constant => mg.insts.items(.data)[index].ty_pl.ty,
-            .add, .sub,
-            .mul, .div, .mod => mg.resolveTy(mg.insts.items(.data)[index].bin_op.lref),
-            .eq, .neq, .geq,
-            .leq, .gt, .lt => mg.resolveTy(mg.insts.items(.data)[index].bin_op.lref),
-            .alloc => mg.insts.items(.data)[index].ty,
-            else => Error.NotImplemented,
-        };
-    } else {
-        return switch (ref) {
-            .zero_val, .one_val => .{ .tag = .comptime_uint },
-            .void_val => .{ .tag = .void },
-            else => Error.NotImplemented,
-        };
-    }
-}
-
 pub fn walkModule(mg: *MirGen) !Mir {
     var analyzer = Analyzer {
         .mg = mg,
