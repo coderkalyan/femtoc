17af8a854c59d8f78b71c98c3a4597446bf517e8
Kalyan Sriram <kalyan@coderkalyan.com>
2023-12-09 22:08:58 -0600

    update test.zig

--- src/test.zig
+++ src/test.zig
@@ -1,12 +1,10 @@
 const std = @import("std");
 
 pub const alu = @import("alu.zig");
-pub const coercion = @import("coercion.zig");
 pub const scope = @import("scope.zig");
 pub const typing = @import("typing.zig");
 pub const value = @import("value.zig");
 pub const lex = @import("lex.zig");
-pub const integerLiteral = @import("integerLiteral.zig");
 
 test {
     std.testing.refAllDecls(@This());
