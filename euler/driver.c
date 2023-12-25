2be660e636e2f7d35d3933142cad97f09ec4f32a
Kalyan Sriram <kalyan@coderkalyan.com>
2023-12-25 09:40:03 -0600

    add euler samples

---
+++ euler/driver.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void print_answer(uint64_t ans) {
+    printf("%lld\n", ans);
+}
+// extern uint64_t solve();
+
+// int main(void) {
+//     printf("%lld\n", solve());
+//     return 0;
+// }
